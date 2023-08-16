0b27c51bfb0f6aeb0a2e051fa5deb88aeb8e9537
Matteo Conti <matt.ciao@gmail.com>
2023-08-16 14:35:06 +0200

    added loading bar

---
+++ R/define_progress_bar.R
@@ -0,0 +1,14 @@
+#' Define a progress bar
+#'
+#' This function creates a progress bar using the `txtProgressBar` function.
+#'
+#' @param n_iterations The number of iterations for the progress bar.
+#' @return The progress bar object.
+define_progress_bar <- function(n_iterations) {
+  pb <- utils::txtProgressBar(min = 0,      # Minimum value of the progress bar
+                              max = n_iterations, # Maximum value of the progress bar
+                              style = 3,    # Progress bar style (also available style = 1 and style = 2)
+                              width = 50,   # Progress bar width. Defaults to getOption("width")
+                              char = "=")
+  return(pb)
+}
