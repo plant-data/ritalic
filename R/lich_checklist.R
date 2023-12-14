4f6e1973b63d34028f8c9f475a2fbd7a675b41ca
Matteo Conti <matt.ciao@gmail.com>
2023-12-14 14:17:52 +0100

    separated distribution from checklist

--- R/lich_checklist.R
+++ R/lich_checklist.R
@@ -1,15 +1,37 @@
 #' @title Lichen checklist
-#' @description This function returns the presence/absence (1 or 0 ) of the lichen species passed as input in each administrative region of Italy.
-#' @param sp_names A vector containing scientific names of lichens.
-#' @return A dataframe with the presence/absence of the lichen species passed as input in each administrative region of Italy
+#' @description This function returns the checklist of the lichen species present in Italy.
+#' @return A vector containing the names of the lichen species present in Italy.
 #' @examples
-#' lich_checklist(c("Cetraria ericetorum Opiz", "Lecanora ciliata"))
+#' lich_checklist()
 #'
 #' @import httr
 #' @import jsonlite
 #'
 #' @export
-lich_checklist <- function(sp_names) {
-  data <- call_api_base(sp_names, "https://italic.units.it/api/v1/checklist/")
-  return(data)
+lich_checklist <- function() {
+ 
+  url <- "https://italic.units.it/api/v1/checklist/"
+  response <- GET(url)
+  
+  # Deal with api errors
+  # 500 server not available (blocks the function)
+  # 429 API usage limit exceeded
+  if (response$status_code == 500) {
+    stop("Impossible to connect to the server, please try again later")
+  } else if (response$status_code == 429) {
+    stop("Impossible to connect to the server, please try again later")
+  } else if (response$status_code == 200) {
+    success <- TRUE
+  } else {
+    stop("An unknown error occurred, please try again later")
+  }
+  
+  # If status_code = 200 everything is fine
+  data <- fromJSON(rawToChar(response$content))
+  
+  checklist <- data[2]
+  checklist <- checklist$checklist
+  
+  return(checklist)
+  
 }
