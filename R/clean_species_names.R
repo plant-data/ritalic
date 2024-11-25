5986c3b257253303c741997d567672196f47fa4c
Matteo Conti <matt.ciao@gmail.com>
2024-11-25 15:22:28 +0100

    fix NA errors in name_match

--- R/clean_species_names.R
+++ R/clean_species_names.R
@@ -6,12 +6,13 @@ clean_species_names <- function(sp_names) {
   if (!is.vector(sp_names, mode = "character")) {
     stop("sp_names must be a character vector", call. = FALSE)
   }
-  replace(sp_names, is.na(sp_names), "")
+  sp_names <- replace(sp_names, is.na(sp_names), "")
   
   # Regular expression to match common Unicode invisible characters
   invisible_char_regex <- "[\u00AD\u034F\u200B-\u200F\u2028-\u202E\u2060-\u206F\uFEFF]"
   
   # Remove invisible characters using gsub
   cleaned_names <- gsub(invisible_char_regex, "", sp_names, perl = TRUE)
+  return(cleaned_names)
 
 }
