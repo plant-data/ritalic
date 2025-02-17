483e19999101fb03e85fab944f611068efe2156e
Matteo Conti <matt.ciao@gmail.com>
2025-02-17 18:47:30 +0100

    now working as intended

--- R/italic_name_usage.R
+++ R/italic_name_usage.R
@@ -4,12 +4,14 @@
 #' Retrieves the morphological description and dditional taxonomic or ecological notes about lichen taxa present in the Checklist of the Lichens of Italy.
 #' Only accepts names that exist in the database of ITALIC.
 #'
-#' @note Before using this function with a list of names, first obtain their accepted names
-#'       using `italic_match()`.
+#' @note Before using this function with a list of names, first obtain their matched names or 
+#'       accepted names using `italic_match()`.
 #'       Example workflow:
 #'       \preformatted{
 #'       names_matched <- italic_match(your_names)
-#'       descriptions <- italic_description(names_matched$accepted_name)
+#'       name_data <- italic_name_usage(names_matched$matched_name)
+#'       # or
+#'       accepted_name_data <- italic_name_usage(names_matched$accepted_name)
 #'       }
 #'
 #' @param sp_names Character vector of accepted names
@@ -23,21 +25,43 @@
 #'
 #' @examples
 #' \dontrun{
-#' italic_description("Cetraria islandica (L.) Ach. subsp. islandica")
+#' italic_name_usage("Cetraria islandica (L.) Ach. subsp. islandica")
 #' }
 #'
 #'
 #' @export
-italic_description <- function(sp_names) {
+italic_name_usage <- function(sp_names) {
   data <-
     call_api_base(
       sp_names,
-      api_endpoint = "https://italic.units.it/api/v1/description/",
-      loading_text = "Retrieving descriptions...",
-      parse_function = parse_api_response,
+      api_endpoint = "https://italic.units.it/api/v1/name-usage/",
+      loading_text = "Retrieving name data...",
+      parse_function = parse_match_response,
       request_method = "GET",
       reorder_result = TRUE
     )
   return(data)
   
+}
+
+#' Parse italic name_usage API response
+#' @param response API response object
+#' @return Parsed dataframe
+#' @noRd
+parse_match_response <- function(response) {
+  data <- fromJSON(rawToChar(response$content))
+  
+  
+  input <- as.data.frame(data['scientific name full'])
+  colnames(input) <- "input_name"
+  fields <- data[1:11]
+  fields <-
+    lapply(fields, function(x)
+      if (is.null(x))
+        NA
+      else
+        x)
+  
+  result <- cbind(input, fields)
+  return(result)
 }
